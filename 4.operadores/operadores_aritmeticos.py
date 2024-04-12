df7d948fdefbe2c574349dd1b279c3b1513cf2bb
user <ulises.a.rivero.garcia@gmail.com>
2024-04-12 13:21:30 -0600

    Added the arithmetic operators exercise

---
+++ 4.operadores/operadores_aritmeticos.py
@@ -0,0 +1,11 @@
+x = 5
+y = 3
+
+suma = x + y
+print("Suma:", suma)
+print("Resta:", x - y)
+print("Multiplicacion:", x * y)
+print("Division:", x / y)
+print("Division entera:", x // y)
+print("Residuo:", x % y)
+print("Potencia:", x ** 2)
