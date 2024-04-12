47c494188673b106a2c8958eeb1e9c3258270046
user <ulises.a.rivero.garcia@gmail.com>
2024-04-12 13:54:28 -0600

    Added the assignment operators exercise

---
+++ 4.operadores/operadores_de_asignacion.py
@@ -0,0 +1,23 @@
+x = 3
+cadena = "Hola "
+tupla = (1, 2, 3)
+
+#x = x + 4
+#cadena = cadena + "Mundo "
+#tupla = tupla + (3, 4, 5)
+x += 4
+cadena += "Mundo "
+tupla += (3, 4, 5)
+
+x -= 2
+x *= 3
+cadena *= 4 #cadena * 4
+
+x /= 3
+x //= 2
+x **= 3
+x %= 3
+
+print("x:", x)
+print("cadena:", cadena)
+print("tupla:", tupla)
