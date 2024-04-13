33f6f0ba03d510b809cf3b4177f2205d74b7dca0
user <ulises.a.rivero.garcia@gmail.com>
2024-04-12 21:50:00 -0600

    Added the logic operators exercise

---
+++ 4.operadores/operadores_logicos.py
@@ -0,0 +1,15 @@
+x = 11
+y = 9
+
+#NOT
+print(x == 11)
+print(not x == 11)
+
+#AND
+print(x == 11 and y == 9)
+print(x == y and y == 9)
+
+#OR
+print(x == y or y == 9)
+print(x == y or y == x)
+print(not x == y or y == x)
