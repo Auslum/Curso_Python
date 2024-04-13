4a1b26cab32b7e6539681bc2abc61dc10bb7ec68
user <ulises.a.rivero.garcia@gmail.com>
2024-04-12 21:57:20 -0600

    Added the membership operators exercise

---
+++ 4.operadores/operadores_de_pertenencia.py
@@ -0,0 +1,15 @@
+'''
+in
+not in
+'''
+
+cadena = "Hola Mundo"
+coleccion = {1, 2, 3,}
+
+print("Mundo" in cadena)
+print("World" in cadena)
+print("world" not in cadena)
+
+print(3 in coleccion)
+print(11 in coleccion)
+print(11 not in coleccion)
