31ec84230c8e7dce40f2dab0dbe71da830cb9789
user <ulises.a.rivero.garcia@gmail.com>
2024-04-24 23:32:00 -0600

    Identity Operators

---
+++ 4.operadores/operadores_de_identidad.py
@@ -0,0 +1,18 @@
+'''
+is
+is not
+'''
+
+numero_1 = 11
+numero_2 = 11
+numero_3 = 33
+set_1 = {1, 2, 3}
+set_2 = {1, 2, 3}
+set_3 = set_1
+
+print("numero_1:", numero_1, id(numero_1))
+print("numero_2:", numero_2, id(numero_2))
+print("numero_3:", numero_3, id(numero_3))
+print("set_1:", set_1, id(set_2))
+print("set_2:", set_2, id(set_2))
+print("set_3:", set_3, id(set_3))
