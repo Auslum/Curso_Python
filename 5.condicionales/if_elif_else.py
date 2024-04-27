4d895ea57f036b6b1d8157f9d4d05b4f286b806a
user <ulises.a.rivero.garcia@gmail.com>
2024-04-26 23:06:53 -0600

    if_elif_else exercise

---
+++ 5.condicionales/if_elif_else.py
@@ -0,0 +1,8 @@
+numero = int(input("Ingresa un numero: "))
+
+if numero > 0:
+    print("El numero es positivo")
+elif numero == 0:
+    print("El numero es neutro")
+else:
+    print("El numero es negativo")
